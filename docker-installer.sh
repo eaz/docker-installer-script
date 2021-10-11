938d192bfdb3d80e58e9bfe2fa9d89ecb3c30ed2
J <julian.romera@gmail.com>
2021-10-11 20:06:41 +0100

    add local port binding

--- docker-installer.sh
+++ docker-installer.sh
@@ -2,14 +2,16 @@
 
 # requires Debian/Ubuntu Linux
 
-# run as: $ curl -s https://raw.githubusercontent.com/eaz/docker-installer-script/main/docker-installer.sh | bash -s 
+# run as: $ curl -s https://raw.githubusercontent.com/eaz/docker-installer-script/main/docker-installer.sh | bash -s
 
-sudo apt update
+sudo apt -y update
 sudo apt upgrade
 
-curl -fsSL https://get.docker.com -o get-docker.sh 
-sh get-docker.sh 
+curl -fsSL https://get.docker.com -o get-docker.sh
+bash -s get-docker.sh
 rm get-docker.sh
+
+sudo sed -i 's+ExecStart=/usr/bin/dockerd+ExecStart=/usr/bin/dockerd -H tcp://127.0.0.1:20202+' /lib/systemd/system/docker.service
 
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
